450576c838b87a2bad7d3c6689a6a8d042368ae5
Gaurav Tank <tankgauravgt@Gauravs-MacBook-Air.local>
2026-06-20 14:16:04 +0530

    updated

--- script.local.sh
+++ script.local.sh
@@ -8,6 +8,8 @@ source .venv/bin/activate
 
 # install dependencies:
 uv pip install -r requirements.txt
+uv pip install pip wheel setuptools ninja packaging
+uv pip install flash-attn-3 --no-deps --index-url https://download.pytorch.org/whl/cu128
 
 # start app:
 python train.py
