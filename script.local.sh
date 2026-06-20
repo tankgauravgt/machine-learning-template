818f5157a7e303e3cbc1ba17a1607ee4ca830d68
Gaurav Tank <tankgauravgt@Gauravs-MacBook-Air.local>
2026-06-20 15:51:01 +0530

    updated

--- script.local.sh
+++ script.local.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
+# create environment:
+UV_VENV_CLEAR=1 uv venv --python 3.12
+
+# activate environment:
+source .venv/bin/activate
+
+# install dependencies:
+uv pip install jarvislabs
+uv pip install tensorboard
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu130
+uv pip install datasets tokenizers transformers pyarrow accelerate
+uv pip install flash-attn-3 --no-deps --index-url https://download.pytorch.org/whl/cu130
+
 # start app:
-python train.py
+.venv/bin/python train.py
