0a0620533677e2bd86c64284bf892f22663c368a
Gaurav Tank <code.tankgauravgt@gmail.com>
2026-07-05 15:32:38 +0530

    Enabled flash attention to speedup
    Updated installation commands and environment variables for CUDA and flash attention.

--- script.local.sh
+++ script.local.sh
@@ -11,12 +11,18 @@ uv pip install jarvislabs
 uv pip install tensorboard
 uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu130
 uv pip install datasets tokenizers transformers pyarrow accelerate
-# uv pip install flash-attn-3 --no-deps --index-url https://download.pytorch.org/whl/cu130
-# wget https://developer.download.nvidia.com/compute/cudnn/redist/cudnn_jit/linux-x86_64/cudnn_jit-linux-x86_64-9.23.1.3_cuda13-archive.tar.xz -O cudnn.tar.xz
-# tar -xf cudnn.tar.xz
-# cp cudnn_jit-linux-x86_64-9.23.1.3_cuda13-archive/include/* /usr/local/cuda/include/
-# cp cudnn_jit-linux-x86_64-9.23.1.3_cuda13-archive/lib/* /usr/local/cuda/lib64/
-# uv pip install "transformer-engine[pytorch]"
+python -m ensurepip --default-pip
+python -m pip install flash-attn-3 --extra-index-url https://download.pytorch.org/whl/cu130
+uv pip install wheel
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/ML/.venv/lib/python3.12/site-packages/nvidia/cudnn/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:/home/ML/.venv/lib/python3.12/site-packages/nvidia/cudnn/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:/home/ML/.venv/lib/python3.12/site-packages/nvidia/cudnn/include
+uv pip install "transformer-engine[pytorch]" --no-build-isolation
+uv pip install nvidia-cublas --extra-index-url=https://download.pytorch.org/whl/cu130 --force-reinstall
+
+# to use flash attention, set `NVTE_FUSED_ATTN=0` and `NVTE_FLASH_ATTN=1`:
+export NVTE_FUSED_ATTN=0
+export NVTE_FLASH_ATTN=1
 
 # start app:
 .venv/bin/python train.py
